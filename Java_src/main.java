66f9271f250d48f538a414430702a2a7536da34a
fslx <51758889+fslx@users.noreply.github.com>
2023-11-05 15:26:54 +0100

    Create main.java

---
+++ Java_src/main.java
@@ -0,0 +1,9 @@
+import Java.lang.System.*;
+
+public class main {
+    public static void main(String[] args) {
+      String x = "Hello World";
+      System.out.println(x);
+      return 0;
+    }
+}
